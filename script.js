6958c31ba8be2f44f08f3cf57cfe1c194daf2a30
username-sam <218394446+username-sam@users.noreply.github.com>
2026-08-10 16:00:14 +0600

    added contact Animation

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let btn = document.querySelector(".btn-follow");
+let contact = document.querySelector('.btn-msg');
+let conInfo = document.querySelector('.contact-info')
+let container = document.querySelector('.card-container')
+
 
 btn.addEventListener('click',function (e) {
   btn.textContent = 'Following'
 })
 
+contact.addEventListener('click',function (e) {
+  conInfo.classList.toggle('show');
+  
+  
+  container.style.animationName = 'fadeIn'
+})
